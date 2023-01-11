33c5eabd1d487426704ca5e15c9844cbba6f074a
Ali M <ali.mo.dev@gmail.com>
2023-01-11 17:44:49 +0330

    created user.schema

---
+++ src/schemas/_index.ts
@@ -0,0 +1,1 @@
+export { createUserSchema } from './user.schema';
---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,28 @@
+import { object, string } from 'zod';
+
+export const createUserSchema = object({
+    body: object({
+
+        email: string({
+            required_error: "Email is required!"
+        }).email("Email is invalid!"),
+
+        username: string({
+            required_error: "Name is required!"
+        }),
+
+        password: string({
+            required_error: "Password is required!"
+        }).min(6, "Password should be between 6 to 25 chars!")
+            .max(25, "Password should be between 6 to 25 chars!"),
+
+        confirmPassword: string({
+            required_error: "Password is required!"
+        }).min(6, "Password should be between 6 to 25 chars!")
+            .max(25, "Password should be between 6 to 25 chars!")
+
+    }).refine((data) => data.password === data.confirmPassword, {
+        message: "Password don't match !",
+        path: ["confirmPassword"]
+    })
+})
