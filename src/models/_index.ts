7a1209f3d0513a3839ee01944230e503b5101a69
Ali M <ali.mo.dev@gmail.com>
2023-01-13 21:26:12 +0330

    created SessionModel

--- src/models/_index.ts
+++ src/models/_index.ts
@@ -1,2 +1,4 @@
 export { default as UserModel } from './user.model';
 export { IUserDocument } from './user.model';
+export { default as SessionModel } from './session.model';
+export { ISessionDocument } from './session.model';
---
+++ src/models/session.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { IUserDocument } from '@models';
+
+export interface ISessionDocument extends mongoose.Document {
+    user: IUserDocument['_id']
+    valid: boolean
+    userAgent: string
+    createdAt: Date
+    updatedAt: Date
+}
+
+const sessionSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    valid: { type: Boolean, default: true },
+    userAgent: { type: String }
+}, {
+    timestamps: true
+});
+const SessionModel = mongoose.model("User", sessionSchema);
+
+export default SessionModel;
