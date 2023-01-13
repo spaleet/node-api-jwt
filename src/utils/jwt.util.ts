68950ebc7a1d5c60eeea447699c7baf648577eef
Ali M <ali.mo.dev@gmail.com>
2023-01-13 22:03:52 +0330

    implemented jwt helper

---
+++ src/utils/jwt.util.ts
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken'
+
+const publicKey: string = process.env.PUBLIC_KEY!
+const privateKey: string = process.env.PRIVATE_KEY!
+
+export function signToken(object: Object, options?: jwt.SignOptions | undefined) {
+    return jwt.sign(object, privateKey, {
+        ...(options && options), // check if undefined
+        algorithm: "RS256"
+    })
+}
+
+export type VerifyJwtResponse = {
+    valid: boolean,
+    expired: boolean,
+    decoded: string | null
+}
+
+export function verifyToken(token: string): VerifyJwtResponse {
+    try {
+        const decoded = jwt.verify(token, publicKey);
+
+        return {
+            valid: true,
+            expired: false,
+            decoded: decoded.toString()
+        }
+    } catch (error: any) {
+        return {
+            valid: false,
+            expired: error.message === 'jwt expired',
+            decoded: null
+        }
+    }
+}
