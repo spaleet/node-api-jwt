a87b48ef1647531c9b973b170294865d1332c4f7
Ali M <ali.mo.dev@gmail.com>
2023-01-29 19:54:58 +0330

    implemented findSessions service

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,4 +1,12 @@
 import { SessionModel } from '@models';
+import { FilterQuery } from 'mongoose';
+import { ISessionDocument } from '../models/session.model';
+
+export async function findSessions(query: FilterQuery<ISessionDocument>) {
+
+    return SessionModel.find(query)
+        .lean(); // remove other methods on the object
+}
 
 export async function createSession(userId: string, userAgent: string) {
     const session = await SessionModel.create({ user: userId, userAgent });
