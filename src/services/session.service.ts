b09c969427678725ecf9bbe8c390bf31493c17ae
Ali M <ali.mo.dev@gmail.com>
2023-02-10 18:02:25 +0330

    implemented updateSession service

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,5 +1,5 @@
 import { SessionModel } from '@models';
-import { FilterQuery } from 'mongoose';
+import { FilterQuery, UpdateQuery } from 'mongoose';
 import { ISessionDocument } from '../models/session.model';
 
 export async function findSessions(query: FilterQuery<ISessionDocument>) {
@@ -12,4 +12,10 @@ export async function createSession(userId: string, userAgent: string) {
     const session = await SessionModel.create({ user: userId, userAgent });
 
     return session.toJSON();
+}
+
+export async function updateSession(sessionId: string, update: UpdateQuery<ISessionDocument>) {
+
+    return SessionModel.updateOne({ _id: sessionId }, update);
+
 }
