170386c416dd882e3741ee6cf8ec07fcdff8bdca
Ali M <ali.mo.dev@gmail.com>
2023-02-11 19:17:24 +0330

    created ProductModel

--- src/models/_index.ts
+++ src/models/_index.ts
@@ -2,3 +2,5 @@ export { default as UserModel } from './user.model';
 export { IUserDocument } from './user.model';
 export { default as SessionModel } from './session.model';
 export { ISessionDocument } from './session.model';
+export { default as ProductModel } from './product.model';
+export { IProductDocument } from './product.model';
---
+++ src/models/product.model.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { IUserDocument } from '@models';
+import { customAlphabet } from 'nanoid';
+
+// generate custom ids
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz0123456789", 10);
+
+export interface IProductDocument extends mongoose.Document {
+    user: IUserDocument['_id']
+    title: string
+    description: string;
+    price: number;
+    image: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const productSchema = new mongoose.Schema({
+    productId: {
+        type: String,
+        required: true,
+        unique: true,
+        default: () => `product_${nanoid()}`,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+}, {
+    timestamps: true
+});
+const ProductModel = mongoose.model<IProductDocument>("Product", productSchema);
+
+export default ProductModel;
